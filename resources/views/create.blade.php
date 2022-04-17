195ab3f3623393c82130ac67f06edb9a6d37bf29
motoki827 <motoki827@gmail.com>
2022-04-17 16:42:25 +0900

    確認画面作成

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
             <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
                 <p class="text-3xl font-bold leading-7 text-center">質問投稿</p>
-                <form action="/" method="post">
+                <form action="/conform" method="post">
                     @csrf
                     <div class="md:flex items-center mt-12">
                         <div class="w-full md:w-1/2 flex flex-col">
